6fbb2987f00b1cecc6bf88aac7fd9f1e1ab717a1
nikkol123 <74326462+nikkol123@users.noreply.github.com>
2026-08-04 01:39:51 +0200

    Add: grumpy-bookstore-owner - submission-0

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        l, r = 0, minutes - 1
+        res = 0
+        while r < len(grumpy):
+            local = 0
+            for i in range(len(grumpy)):
+                if i >=l and i<=r:
+                    local += customers[i]
+                else:
+                    if not grumpy[i]:
+                        local += customers[i]
+            l+=1
+            r+=1
+            res = max(res, local)
+        return res
+            
+
+
+
+
