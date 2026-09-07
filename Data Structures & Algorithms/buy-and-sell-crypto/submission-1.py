82a299e3427854a928a9137bdadd9c13e71cc3eb
nikkol123 <74326462+nikkol123@users.noreply.github.com>
2026-09-08 00:56:00 +0100

    Update: buy-and-sell-crypto - submission-1

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -1,9 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        localMax = 0
-        buy = prices[0]
-        for price in prices:
-            profit = price - buy
-            if profit > localMax: localMax = profit
-            if price < buy: buy = price
-        return localMax
+        # res = 0
+        # minPrice = float("inf")
+        # for price in prices:
+        #     profit = price - minPrice
+        #     res = max(profit, res)
+        #     minPrice = min(minPrice, price)
+        # return res
+
+        left, right = 0, 1
+        maxProfit = 0
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right+=1
+        return maxProfit
+
+
+
+
+
